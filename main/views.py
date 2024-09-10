9a09ea0e9822521cbd116006c2f684394c48953e
arzakaraffan <arzakaraffan@gmail.com>
2024-09-10 19:56:49 +0700

    HTML Updated

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ def show_main(request):
     context = {
         'itemName': 'Chet Baker Vinyl - Sings Again 1985 (Reissued)',
         'itemPrice' : 'Rp599.000,00',
-        'itemDescription' : 'Artist: Chet Baker\n    Album: Sings Again\n    Country: Europe\n    Genre: Jazz\n    Style: Bop, Cool Jazz',
+        'itemDescription' : 'Artist: Chet Baker\nAlbum: Sings Again\nCountry: Europe\nGenre: Jazz\nStyle: Bop, Cool Jazz',
         'itemStock' : 2
         }
 
