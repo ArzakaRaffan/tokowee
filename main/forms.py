f28eda8afea5f155a3295477d1e497f76831fbbd
arzakaraffan <arzakaraffan@gmail.com>
2024-10-06 12:01:32 +0700

    Tugas 6 Commit

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,32 @@
 from django.forms import ModelForm
 from main.models import Product
+from django.utils.html import strip_tags
 
 class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = ['itemName', 'itemDescription', 'itemPrice', 'itemStock', 'itemCategory', 'itemImageURL']
+
+    def clean_itemName(self):
+        itemName = self.cleaned_data["itemName"]
+        return strip_tags(itemName)
+    
+    def clean_itemDescription(self):
+        itemDescription = self.cleaned_data["itemDescription"]
+        return strip_tags(itemDescription)
+    
+    def clean_itemPrice(self):
+        itemPrice = self.cleaned_data["itemPrice"]
+        return strip_tags(itemPrice)
+    
+    def clean_itemStock(self):
+        itemStock = self.cleaned_data["itemStock"]
+        return strip_tags(itemStock)
+    
+    def clean_itemCategory(self):
+        itemCategory = self.cleaned_data["itemCategory"]
+        return strip_tags(itemCategory)
+    
+    def clean_itemImageURL(self):
+        itemImageURL = self.cleaned_data["itemImageURL"]
+        return strip_tags(itemImageURL)
--- main/views.py
+++ main/views.py
@@ -9,17 +9,13 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 import datetime
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
 
 @login_required(login_url='/login')
 def show_main(request):
-    products =  Product.objects.filter(user=request.user)
     context = {
-        'itemName': 'Chet Baker Vinyl - Sings Again 1985 (Reissued)',
-        'itemPrice' : 'Rp599.000,00',
-        'itemDescription' : 'Artist: Chet Baker\nAlbum: Sings Again\nCountry: Europe\nGenre: Jazz\nStyle: Bop, Cool Jazz',
-        'itemStock' : 2,
-        'itemCategory': 'Music',
-        'products': products,
         'last_login': request.COOKIES.get('last_login', 'Never'),
         'username': request.user.username
         }
@@ -42,11 +38,11 @@ def create_product(request):
 
 
 def show_xml(request):
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
@@ -79,6 +75,8 @@ def login_user(request):
             response = HttpResponseRedirect(reverse("main:show_main"))
             response.set_cookie('last_login', str(datetime.datetime.now()))
             return response
+        else:
+            messages.error(request, "Invalid username or password. Please try again.")
     else:
         form = AuthenticationForm(request)
 
@@ -111,3 +109,25 @@ def delete_product(request, id):
     product = Product.objects.get(pk = id)
     product.delete()
     return HttpResponseRedirect(reverse('main:show_main'))
+
+@csrf_exempt
+@require_POST
+def add_product_ajax(request):
+    itemName = strip_tags(request.POST.get("itemName"))
+    itemDescription = strip_tags(request.POST.get("itemDescription"))
+    itemPrice = strip_tags(request.POST.get("itemPrice"))
+    itemStock = strip_tags(request.POST.get("itemStock"))
+    itemImageURL = strip_tags(request.POST.get("itemImageURL"))
+    itemCategory = strip_tags(request.POST.get("itemCategory"))
+    user = request.user
+
+    new_product = Product(
+        itemName = itemName, itemDescription = itemDescription,
+        itemPrice = itemPrice, itemStock = itemStock,
+        itemImageURL = itemImageURL, itemCategory = itemCategory,
+        user = user
+    )
+
+    new_product.save()
+
+    return HttpResponse(b"CREATED", status=201)
