d1ccb9b899aca13de93247f8e533c3c9c6405eb9
arzakaraffan <arzakaraffan@gmail.com>
2024-10-26 21:06:42 +0700

    feat: Admin View Implemented (Create, Edit, and Delete)

--- main/models.py
+++ main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import uuid
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
-
+    
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -12,3 +12,6 @@ class Product(models.Model):
     itemStock = models.IntegerField(validators=[MinValueValidator(0)])
     itemImageURL = models.URLField(default='https://i.imgur.com/tGd7up6.jpeg')
     itemCategory = models.TextField()
+
+    def __str__(self):
+        return self.itemName
